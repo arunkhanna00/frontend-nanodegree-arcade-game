c14307a26170d97d6e389bd848bbad99f85c67c2
Arun Khanna <arunkhanna00@gmail.com>
2016-06-10 14:22:52 +0530

    start game functionality and allow player to move also setup the enemies with random speeds and positions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// TODO: Add a collision mechanism
+// TODO: Add a score count and other features
+
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -5,7 +9,13 @@ var Enemy = function() {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = 'images/enemy-bug.png'; // Setup enemy image
+    this.x = 0; // Start the x value of the enemy at 0
+    enemyRows = [60, 140, 230]; // Create an array of rows that the Enemy can start on
+    enemySpeeds = [200, 300, 400, 500, 600] // Create an array of speeds that can be randomly chosen
+    this.y = enemyRows[Math.floor(Math.random()*3)]; // Setup a random lane
+    this.speed = enemySpeeds[Math.floor(Math.random()*5)]; // Setup a random speed
+    this.addNewEnemy = true; // Setup a boolean variable corresponding to if an enemy has been added or not
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +24,18 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += dt * this.speed; // Update x value accoding to speed and dt
+    if (Math.floor(this.x) >= 200 && this.addNewEnemy) {
+        // Add a new enemy instance after an enmy crosses
+        // 200 pixels
+        allEnemies.push(new Enemy());
+        this.addNewEnemy = false;
+    }
+    if (this.x > 1500) {
+        // Remove an enemy after it has exited the screen
+        // completely
+        allEnemies.shift();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +46,46 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png'; // Setup image of Player
+    this.x = 200; // Setup initial x position of Player
+    this.y = 400; // Setup initial y position of Player
+};
 
+Player.prototype.update = function() {};
 
+Player.prototype.handleInput = function(key) {
+    if (key == 'up') {
+        // Setup up key to move player up
+        this.y -= 90;
+        if (this.y <= 0) {
+            // Reset player if up key would move player into water
+            this.x = 200;
+            this.y = 400
+        }
+    }
+    else if (key == 'down' && this.y <= 310) {
+        // Setup down key to move player down if inside screen
+        this.y += 90;
+    }
+    else if (key == 'left' && this.x >= 85) {
+        // Setup left key to move player left if inside screen
+        this.x -= 100;
+    }
+    else if (key == 'right' && this.x <= 315) {
+        // Setup right key to move player right if inside screen
+        this.x += 100;
+    }
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+allEnemies = [new Enemy()]; // Setup an array of Enemies
+player = new Player(); // Setup Player
 
 
 
